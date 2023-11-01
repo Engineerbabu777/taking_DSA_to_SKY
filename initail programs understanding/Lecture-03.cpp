2d312147ce4dcca224bffe36f10933b56b5912ab
Engineerbabu777 <awaismumtaz0099@gmail.com>
2023-11-01 17:38:41 +0500

    'day-3'

---
+++ initail programs understanding/Lecture-03.cpp
@@ -0,0 +1,148 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	
+	
+//	PROGRAM 01-> 
+	// 1 2 3 4
+	// 1 2 3 4
+	// 1 2 3 4
+	
+//	int i = 1;
+//	while(i<=4){
+//		int j = 1;
+//		
+//		while(j<=4){
+//			cout<<" "<<j<<" ";
+//			j++;
+//		}
+//		cout<<endl;
+//		i++;
+//	}
+
+//	PROGRAM 02-> 
+
+//cout<<"PATTREN 02"<<endl;
+// 3 2 1
+// 3 2 1
+
+//int a = 0;
+//while(a<4){
+//	int b = 3;
+//	while(b>=0){
+//		cout<<" "<<b<<" ";
+//		b--;
+//	}
+//	cout<<endl;
+//	a++;
+//}
+
+
+//	PROGRAM 03->
+// 1 2 3 4 5
+// 6 7 8 9 10 ...
+
+//int count=1;
+//int i = 0;
+//
+//while(i<30){
+//	
+//	
+//	int j = 0;
+//	while(j<3){
+//		cout<<" "<<count;
+//		count++;
+//		j++;
+//	}
+//	i++;
+//	cout<<endl;
+//} 
+
+
+
+
+//	PROGRAM 04->
+
+// *
+// **
+// ***
+
+//int size;
+//char ch;
+//cout<<"Enter Size: "<<endl;
+//cin>>size;
+//cout<<"Enter Symbol: "<<endl;
+//cin>>ch;
+//int i=0;
+//while(i<size){
+//	int j = 0;
+//	while(j <= i){
+//		cout<<" "<<"*";
+//		j++;
+//	}
+//	cout<<endl;
+//	i++;
+//}
+
+
+
+//	PROGRAM 05->
+// 1
+// 2 2
+// 3 3 3
+//int size;
+//cout<<"Enter Size: "<<endl;
+//cin>>size;
+//int i=0;
+//while(i<size){
+//	int j = 0;
+//	while(j <= i){
+//		cout<<" "<<i+1;
+//		j++;
+//	}
+//	cout<<endl;
+//	i++;
+//}
+
+
+
+//	PROGRAM 06-> 
+// 1
+// 2 3
+// 4 5 6
+//int count=1;
+//int i = 0;
+//
+//while(i<10){
+//	
+//	
+//	int j = 0;
+//	while(j<=i){
+//		cout<<" "<<count;
+//		count++;
+//		j++;
+//	}
+//	i++;
+//	cout<<endl;
+//} 
+
+
+
+//	PROGRAM 07-> 
+
+//	PROGRAM 08-> 
+
+//	PROGRAM 09-> 
+
+//	PROGRAM 10-> 
+
+//	PROGRAM 11-> 
+
+//	PROGRAM 11->    
+
+
+
+return 0;
+}
+
